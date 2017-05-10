3c0b8caf541d991aa5082b66ba3b47484e3abdfa
Guilherme Moraes <guilherme.moraes@outlook.com>
2017-05-09 23:06:24 -0300

    Creating Pause menu frame in another thread

--- src/main/java/br/com/guigasgame/solitaire/gamemachine/PauseState.java
+++ src/main/java/br/com/guigasgame/solitaire/gamemachine/PauseState.java
@@ -3,6 +3,8 @@ package br.com.guigasgame.solitaire.gamemachine;
 
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import br.com.guigasgame.solitaire.gui.MenuOptionsFrame;
 
@@ -13,15 +15,21 @@ public class PauseState implements GameState, WindowListener
 
 	public PauseState()
 	{
-		pauseFrame = new MenuOptionsFrame();
-		pauseFrame.setVisible(true);
-		pauseFrame.addWindowListener(this);
+		Runnable task = () ->
+		{
+
+			pauseFrame = new MenuOptionsFrame();
+			pauseFrame.setVisible(true);
+			pauseFrame.addWindowListener(this);
+		};
+		ExecutorService executorScoreAdd = Executors.newFixedThreadPool(1);
+		executorScoreAdd.submit(task);
 	}
 	
 	@Override
 	public void update(float updateDelta)
 	{
-		if (markToSwitchState)
+		if (pauseFrame != null && markToSwitchState)
 		{
 			GameMachine.getInstance().popState();
 			if (pauseFrame.requiresNewGame())
